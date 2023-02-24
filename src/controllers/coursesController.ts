1492526fec92a7ced9e0450d558cc4fee94b66e9
Guilherme Bressanim <guilhermeacb@hotmail.com>
2023-02-24 15:31:05 -0300

    Obtendo 3 cursos em destaque

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,6 +2,18 @@ import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
 
 export const coursesController = {
+    featured: async (req: Request, res: Response) => {
+
+        try {
+            const featuresCourses = await courseService.getRandomFeaturedCourses()
+            return res.json(featuresCourses)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
     show: async (req: Request, res: Response) => {
         const { id } = req.params
 
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -10,7 +10,7 @@ export const courseService = {
                 ['thumbnail_url', 'thumbnailUrl']
             ],
             include: {
-                association: 'Episodes',
+                association: 'episodes',
                 attributes: [
                     'id',
                     'name',
@@ -25,5 +25,23 @@ export const courseService = {
         })
 
         return courseWithEpisodes
+    },
+
+    getRandomFeaturedCourses: async () => {
+        const featuredCourses = await Course.findAll({
+            attributes: [
+                'id',
+                'name',
+                'synopsis',
+                ['thumbnail_url', 'thumbnailUrl']
+            ],
+            where: {
+                featured: true
+            }
+        })
+
+        const randomFeaturesCourses = featuredCourses.sort(() => 0.5 - Math.random())
+
+        return randomFeaturesCourses.slice(0, 3)
     }
 }

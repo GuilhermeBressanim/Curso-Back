88e4a3a4223a8ab9fa744a8357a041ef40ee3611
Guilherme Bressanim <guilhermeacb@hotmail.com>
2023-02-24 15:13:17 -0300

    Obtendo cursos de uma categoria

---
+++ src/controllers/categoriesController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { getPaginationParams } from "../helpers/getPaginationParams";
+import { categoryService } from "../services/categoryService";
+
+export const categoriesController = {
+    index: async (req: Request, res: Response) => {
+        const [page, perPage] = getPaginationParams(req.query)
+
+        try {
+            const paginatedCategories = await categoryService.findAllPaginated(page, perPage)
+
+            return res.json(paginatedCategories)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+    show: async (req: Request, res: Response) => {
+        const { id } = req.params
+
+        try {
+            const category = await categoryService.findByIdWithCourses(id)
+            return res.json(category)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    }
+}
---
+++ src/services/categoryService.ts
@@ -0,0 +1,38 @@
+import { Category } from "../models"
+
+export const categoryService = {
+    findAllPaginated: async (page: number, perPage: number) => {
+        const offset = (page - 1) * perPage
+
+        const { count, rows } = await Category.findAndCountAll({
+            attributes: ['id', 'name', 'position'],
+            order: [['position', 'ASC']],
+            limit: perPage,
+            offset: offset
+        })
+
+        return {
+            categories: rows,
+            page: page,
+            perPage: perPage,
+            total: count
+        }
+    },
+
+    findByIdWithCourses: async (id: string) => {
+        const categoryWithCourses = await Category.findByPk(id, {
+            attributes: ['id', 'name'],
+            include: {
+                association: 'courses',
+                attributes: [
+                    'id',
+                    'name',
+                    'synopsis',
+                    ['thumbnail_url', 'thumbnailUrl']
+                ]
+            }
+        })
+
+        return categoryWithCourses
+    }
+}

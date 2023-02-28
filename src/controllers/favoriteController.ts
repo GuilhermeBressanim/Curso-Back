284eb1ea65300ced98aa6ebd0c3793e58833f1e9
Guilherme Bressanim <guilhermeacb@hotmail.com>
2023-02-28 14:10:10 -0300

    Obtendo os cursos favoritos

--- src/controllers/favoriteController.ts
+++ src/controllers/favoriteController.ts
@@ -3,6 +3,19 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { favoriteService } from "../services/favoriteService";
 
 export const favoritesController = {
+    index: async (req: AuthenticatedRequest, res: Response) => {
+        const userId = req.user!.id
+
+        try {
+            const favorites = await favoriteService.findByUserId(userId)
+            return res.json(favorites)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
     save: async (req: AuthenticatedRequest, res: Response) => {
         const userId = req.user!.id
         const { courseId } = req.body
@@ -16,4 +29,6 @@ export const favoritesController = {
             }
         }
     }
+
+
 }

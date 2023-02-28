03128846b7a3d67b391500c1d61627621bd4fd57
Guilherme Bressanim <guilhermeacb@hotmail.com>
2023-02-28 17:22:50 -0300

    Atualizando o endpoint de cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { getPaginationParams } from "../helpers/getPaginationParams";
+import { AuthenticatedRequest } from "../middlewares/auth";
 import { courseService } from "../services/courseService";
+import { favoriteService } from "../services/favoriteService";
+import { likeService } from "../services/likeService";
 
 export const coursesController = {
     featured: async (req: Request, res: Response) => {
@@ -41,12 +44,18 @@ export const coursesController = {
 
 
 
-    show: async (req: Request, res: Response) => {
-        const { id } = req.params
+    show: async (req: AuthenticatedRequest, res: Response) => {
+        const userId = req.user!.id
+        const courseId = req.params.id
 
         try {
-            const course = await courseService.findByIdWithEpisodes(id)
-            return res.json(course)
+            const course = await courseService.findByIdWithEpisodes(courseId)
+
+            if (!course) return res.status(404).json({ message: 'Curso não encontrado.' })
+
+            const liked = await likeService.isLiked(userId, Number(courseId))
+            const favorited = await favoriteService.isFavorited(userId, Number(courseId))
+            return res.json({ ...course.get(), liked, favorited })
         } catch (err) {
             if (err instanceof Error) {
                 return res.status(400).json({ message: err.message })
---
+++ src/services/likeService.ts
@@ -0,0 +1,32 @@
+import { Like } from "../models"
+
+export const likeService = {
+    create: async (userId: number, courseId: number) => {
+        const like = await Like.create({
+            userId,
+            courseId
+        })
+
+        return like
+    },
+
+    delete: async (userId: number, courseId: number) => {
+        await Like.destroy({
+            where: {
+                userId,
+                courseId
+            }
+        })
+    },
+
+    isLiked: async (userId: number, courseId: number) => {
+        const like = await Like.findOne({
+            where: {
+                userId,
+                courseId
+            }
+        })
+
+        return like !== null ? true : false
+    }
+}

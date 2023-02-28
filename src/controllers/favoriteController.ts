3955db397f3aea4db45c027df0dea10d76820455
Guilherme Bressanim <guilhermeacb@hotmail.com>
2023-02-28 14:01:41 -0300

    Adicionando favoritos (minha lista)

---
+++ src/controllers/favoriteController.ts
@@ -0,0 +1,19 @@
+import { Response } from "express";
+import { AuthenticatedRequest } from "../middlewares/auth";
+import { favoriteService } from "../services/favoriteService";
+
+export const favoritesController = {
+    save: async (req: AuthenticatedRequest, res: Response) => {
+        const userId = req.user!.id
+        const { courseId } = req.body
+
+        try {
+            const favorite = await favoriteService.create(userId, Number(courseId))
+            return res.status(201).json(favorite)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    }
+}

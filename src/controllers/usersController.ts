99f8beb9c023690491e15f0c9ec46e1b5325a7e7
Guilherme Bressanim <guilhermeacb@hotmail.com>
2023-03-01 14:45:39 -0300

    Obtendo informações da conta do usuário

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -3,6 +3,17 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { userService } from "../services/userService";
 
 export const usersController = {
+    show: async (req: AuthenticatedRequest, res: Response) => {
+        try {
+            const currentUser = req.user!
+            return res.json(currentUser)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
     watching: async (req: AuthenticatedRequest, res: Response) => {
         const { id } = req.user!
 
